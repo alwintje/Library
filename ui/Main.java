f955f3c5ef55c25dcbb665a008ad377c13f539e0
Yosh98 <Yosh98@gmx.com>
2018-04-12 15:57:04 +0200

    UI shizzle toegevoegd

--- ui/Main.java
+++ ui/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("startscreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("startScreen.fxml"));
         primaryStage.setTitle("Library");
         primaryStage.setScene(new Scene(root, 960, 540));
         primaryStage.show();
