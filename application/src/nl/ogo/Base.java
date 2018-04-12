91c690d85ba805ce58c371706f111b40b02db304
alwintje <alwinkroesen@gmail.com>
2018-04-12 13:33:35 +0200

    application

---
+++ application/src/nl/ogo/Base.java
@@ -0,0 +1,16 @@
+package nl.ogo;
+
+
+public class Base{
+
+        public Base(){
+            System.out.println();
+            System.out.println("  BASE CLASS");
+            System.out.println();
+        }
+
+        public static void main(String[] args) {
+            new Base();
+        }
+
+}
