11f27886b50648e8f75117f3f53ce25315f65876
Yosh98 <Yosh98@gmx.com>
2018-04-13 15:26:02 +0200

    Complete UI toegevoegd. Links naar code missen.

--- ui/Controller.java
+++ ui/Controller.java
@@ -1,4 +1,83 @@
+/**
+ * Sample Skeleton for 'userBorrowItem.fxml' Controller Class
+ */
+
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Controller {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+
+
+    // User borrow items screen
+    @FXML
+    void userBorrowAddItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userFinishBorrowItems(ActionEvent event) {
+
+    }
+
+
+    // User reservate items screen
+    @FXML
+    void userReservateSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userReservateAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userReservateDone(ActionEvent event) {
+
+    }
+
+    // User home screen
+    @FXML
+    void userHomeItemsDeposit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userHomeItemsBorrow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userHomeReservate(ActionEvent event) {
+
+    }
+
+
+    // User deposit items screen
+    @FXML
+    void userDepositAddItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userDepositDone(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
 }
